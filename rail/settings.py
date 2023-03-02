0d6103faf6620c4d50665509f2d443195f643f5a
Joe Muraguri <joe.crrm@gmail.com>
2023-03-02 15:39:11 +0300

    admin update

--- rail/settings.py
+++ rail/settings.py
@@ -122,3 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS=['https://railway-test-production-dc73.up.railway.app/']
+#https://railway-test-production-dc73.up.railway.app/
